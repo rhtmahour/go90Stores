e98473d4fa23a3598968844b5b6773174ecdb1fd
rhtmahour <rhtmahour@gmail.com>
2025-04-22 18:29:09 +0530

    code on date 22-04-2025

--- android/app/src/main/kotlin/com/example/go90stores/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.go90stores
-
-import io.flutter.embedding.android.FlutterFragmentActivity 
-
-class MainActivity: FlutterFragmentActivity ()
---
+++ android/app/src/main/kotlin/com/go90stores/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.go90stores
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity()
