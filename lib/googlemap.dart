0c2a6951d120d3798d7b934d63cd4da3e35c9aac
rhtmahour <rhtmahour@gmail.com>
2025-05-20 18:40:45 +0530

    code on date 20-05-2025

--- lib/googlemap.dart
+++ lib/googlemap.dart
@@ -11,21 +11,58 @@ class GoogleMapScreen extends StatefulWidget {
 class _GoogleMapScreenState extends State<GoogleMapScreen> {
   Completer<GoogleMapController> _controller = Completer();
   static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(28.688384, 77.2079616),
+    target: LatLng(28.5310729, 77.0857176),
     zoom: 14,
   );
+  List<Marker> _markers = [];
+  final List<Marker> _list = const [
+    Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(28.5310729, 77.0857176),
+        infoWindow: InfoWindow(
+          title: 'Go90 mart',
+        )),
+  ];
+  @override
+  void initState() {
+    super.initState();
+    _markers.addAll(_list);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: _kGooglePlex,
-        mapType: MapType.normal,
-        compassEnabled: true,
-        myLocationEnabled: true,
-        myLocationButtonEnabled: false,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+      body: SafeArea(
+        child: GoogleMap(
+          markers: Set<Marker>.of(_markers),
+          initialCameraPosition: _kGooglePlex,
+          mapType: MapType.normal,
+          compassEnabled: true,
+          myLocationEnabled: true,
+          myLocationButtonEnabled: false,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.location_disabled_outlined),
+        onPressed: () async {
+          GoogleMapController controller = await _controller.future;
+          controller.animateCamera(CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: LatLng(39.000000, -80.500000),
+              zoom: 14,
+            ),
+          ));
+          setState(() {
+            _markers.add(Marker(
+                markerId: MarkerId('2'),
+                position: LatLng(39.000000, -80.500000),
+                infoWindow: InfoWindow(
+                  title: 'New Location',
+                )));
+          });
         },
       ),
     );
